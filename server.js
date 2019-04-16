07e21d98bf2b4efe8059bc43b456ce9c9a102f32
IM11 신현수 <kilcmn@gmail.com>
2019-04-16 13:13:23 +0900

    complete  API

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import { users, tests } from './routes';
 
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
@@ -18,3 +19,49 @@ app.use(tests);
 
 app.set('port', port);
 app.listen(app.get('port'));
+
+//cors() - CORS를 대응하기 위한 라이브러리?
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    method: ['GET', 'POST'],
+    credentials: true
+  })
+);
+
+app.get('/', (req, res) => {
+  res.status(200).send('Success');
+});
+
+app.post('/users/singup', (req, res) => {
+  const { username, password } = req.body;
+  users
+    .create({
+      username: username,
+      password: password
+    })
+    .then(result => {
+      res
+        .status(200)
+        .json(result)
+        .end('OK');
+    })
+    .catch(error => {
+      console.log(error);
+      res.sendStatus(400);
+    });
+});
+
+app.post('/users/signin', (req, res) => {
+  // Search for a specific element or create it if not available
+  return Customers.findOrCreate({
+    where: {
+      username: customers.username
+    },
+    defaults: {
+      username: customers.username
+    }
+  });
+});
+
+module.exports = app;
