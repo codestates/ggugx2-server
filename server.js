4af219b7efc833610429dcde09bfe5078839ab67
Seung-Yoon Oh <feynubrick@gmail.com>
2019-04-18 11:06:15 +0900

    added test client and connection enabled

--- server.js
+++ server.js
@@ -2,13 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { users, tests } from './routes';
 import cors from 'cors';
-// import socketIO from 'socket.io';
+import socketIO from 'socket.io';
 import http from 'http';
 
 const app = express();
 const server = http.createServer(app);
-// const io = socketIO(server);
 const port = process.env.PORT || 3000;
+const io = socketIO(server);
+
+const registeredSockets = {};
+io.on('connection', socket => {
+  console.log('a socket connected!');
+  socket.on('register', msg => {
+    registeredSockets[msg.id] = socket;
+    socket.id = msg.id;
+    console.log(`${socket.id} has been registered!`);
+  });
+});
 
 app.get('/', (req, res) => {
   console.log('request reached at /');
@@ -17,11 +27,13 @@ app.get('/', (req, res) => {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+// app.use(cors());
 
 app.use(users);
 app.use(tests);
 
 app.set('port', port);
 // app.listen(app.get('port'));
-server.listen(app.get('port'));
+server.listen(app.get('port'), () => {
+  console.log('now listening on port ', app.get('port'));
+});
