3d29982f7ffe901ced7c23ca84c21ef5707bbc33
Seung-Yoon Oh <feynubrick@gmail.com>
2019-04-18 11:09:44 +0900

    added and tested public chat and private chat

--- server.js
+++ server.js
@@ -18,6 +18,22 @@ io.on('connection', socket => {
     socket.id = msg.id;
     console.log(`${socket.id} has been registered!`);
   });
+
+  socket.on('private', msg => {
+    console.log(
+      `[private] ${socket.id} send a message to ${msg.to}: ${msg.message}`
+    );
+    registeredSockets[msg.to].emit('private', {
+      from: socket.id,
+      message: msg.message
+    });
+    socket.emit('private', { from: socket.id, message: msg.message });
+  });
+
+  socket.on('public chat', msg => {
+    console.log(`[public chat] message from ${socket.id}: ${msg}`);
+    io.emit('public chat', { id: socket.id, message: msg });
+  });
 });
 
 app.get('/', (req, res) => {
