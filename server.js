fd401451950da7d3f702c91a934939118f1295a7
Seung-Yoon Oh <feynubrick@gmail.com>
2019-04-18 14:20:01 +0900

    added store client and edited to send add stamp message from customer to store

--- server.js
+++ server.js
@@ -19,20 +19,23 @@ io.on('connection', socket => {
     console.log(`${socket.id} has been registered!`);
   });
 
-  socket.on('stamp add', msg => {
+  socket.on('stamp add from user', msg => {
     console.log(`[stamp add] ${socket.id} send a request to ${msg.store}`);
-    socket.emit('stamp complete', {
-      customer: socket.id,
-      store: msg.store,
-      success: true
+    registeredSockets[msg.store].emit('stamp confirm to store', {
+      customer: socket.id
     });
+    // socket.emit('stamp complete', {
+    //   customer: socket.id,
+    //   store: msg.store,
+    //   success: true
+    // });
     socket.emit('private', { from: socket.id, message: msg.message });
   });
 
-  socket.on('public chat', msg => {
-    console.log(`[public chat] message from ${socket.id}: ${msg}`);
-    io.emit('public chat', { id: socket.id, message: msg });
-  });
+  // socket.on('stamp confirm', msg => {
+  //   console.log(`[stamp confirm] stamp request from ${socket.id}: ${msg}`);
+  //   io.emit('public chat', { id: socket.id, message: msg });
+  // });
 });
 
 app.get('/', (req, res) => {
