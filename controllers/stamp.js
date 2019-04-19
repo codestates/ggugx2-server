bbe517b11e4f0798687003e350a1ccac60b4d50c
Seung-Yoon Oh <feynubrick@gmail.com>
2019-04-19 16:29:41 +0900

    adding stamp to db works on specific situation

--- controllers/stamp.js
+++ controllers/stamp.js
@@ -1,27 +1,40 @@
+import db from '../models';
 const registeredSockets = {};
 
 const stamp = function(socket) {
   console.log('a socket connected!');
-  socket.on('register', msg => {
-    registeredSockets[msg.id] = socket;
-    socket.id = msg.id;
-    console.log(`${socket.id} has been registered!`);
-  });
-
-  socket.on('stamp add from user', msg => {
-    console.log(`[stamp add] ${socket.id} send a request to ${msg.store}`);
-    registeredSockets[msg.store].emit('stamp confirm to store', {
-      customer: socket.id
+  try {
+    socket.on('register', msg => {
+      registeredSockets[msg.id] = socket;
+      socket.id = msg.id;
+      console.log(`${socket.id} has been registered!`);
     });
-  });
 
-  socket.on('stamp confirm from store', msg => {
-    console.log(
-      `[stamp confirm] ${socket.id} confirm stamp add for ${msg.customer}`
-    );
-    registeredSockets[msg.customer].emit('stamp add complete', msg);
-    socket.emit('stamp add complete', msg);
-  });
+    socket.on('stamp add from user', msg => {
+      console.log(`[stamp add] ${socket.id} send a request to ${msg.store}`);
+      registeredSockets[msg.store].emit('stamp confirm to store', {
+        customer: socket.id
+      });
+    });
+
+    socket.on('stamp confirm from store', async msg => {
+      console.log(
+        `[stamp confirm] ${socket.id} confirm stamp add for ${msg.customer}`
+      );
+
+      db.stamps.create({
+        CUSTOMER_ID: msg.customer,
+        STORE_ID: socket.id
+      });
+
+      registeredSockets[msg.customer].emit('stamp add complete', msg);
+      socket.emit('stamp add complete', msg);
+    });
+  } catch (err) {
+    socket.on('errors', {
+      message: err.message
+    });
+  }
 };
 
 export default stamp;
