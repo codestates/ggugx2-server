5953d278c6c61e60c4608b806a47b59e74615986
Seung-Yoon Oh <feynubrick@gmail.com>
2019-04-22 14:14:58 +0900

    created an api to check whether customer exists

---
+++ controllers/doesCustomerExist.js
@@ -0,0 +1,28 @@
+import db from '../models';
+import { USER_NOT_EXISTS } from '../errorMessages';
+
+const doesCustomerExist = async (req, res) => {
+  const phone = req.body.phone;
+  try {
+    const searchResult = await db.customers.findOne({
+      where: { PHONE: phone }
+    });
+
+    if (!searchResult) {
+      throw new Error(USER_NOT_EXISTS);
+    }
+
+    const customer = searchResult.dataValues;
+
+    console.log('customer found: ', customer);
+    res.status(200).json({ id: customer.id });
+  } catch (err) {
+    if (err.message === USER_NOT_EXISTS) {
+      res.status(400).send(err.message);
+    } else {
+      res.status(500).send(err.message);
+    }
+  }
+};
+
+export default doesCustomerExist;
--- controllers/index.js
+++ controllers/index.js
@@ -4,3 +4,4 @@ export { default as signupStores } from './signupStores';
 export { default as signinStores } from './signinStores';
 export { default as socketioHandler } from './socketio';
 export { default as exchange } from './exchange';
+export { default as doesCustomerExist } from './doesCustomerExist';
