7ec6b7f34c22bf1630bb9dc0ec610dd4209f8bf7
IM11 신현수 <kilcmn@gmail.com>
2019-04-23 10:52:37 +0900

    complete creating API for querying store info

--- controllers/getStoreInfo.js
+++ controllers/getStoreInfo.js
@@ -3,31 +3,37 @@ import db from '../models';
 const getStoreInfo = async (req, res) => {
   const storeID = req.body.storeID;
 
+  if (!storeID) {
+    throw new Error('storeID is missing, check storeID again please');
+  }
+
   try {
     let storeInfo = await db.stores.findOne({
       attributes: ['ADDRESS', 'OPENHOUR', 'CLOSEHOUR', 'PHONE', 'DAYOFF'],
       where: { id: storeID }
     });
 
-    // let { dataValues } = storeInfo;
-    // let store = dataValues;
-    // console.log('!!!!!!!!!!!!!!!!!!!!!', store);
-
-    // if (stroeID === store.STORE_ID) {
-    res.status(200).json({
-      address: storeInfo.ADDRESS,
-      openhour: storeInfo.OPENHOUR,
-      closehour: storeInfo.CLOSEHOUR,
-      contact: storeInfo.PHONE,
-      dayoff: storeInfo.DAYOFF
-    });
+    if (
+      storeInfo.ADDRESS !== null &&
+      storeInfo.OPENHOUR !== null &&
+      storeInfo.CLOSEHOUR !== null &&
+      storeInfo.PHONE !== null &&
+      storeInfo.DAYOFF !== null
+    ) {
+      res.status(200).json({
+        storeInfos: {
+          address: storeInfo.ADDRESS,
+          openhour: storeInfo.OPENHOUR,
+          closehour: storeInfo.CLOSEHOUR,
+          contact: storeInfo.PHONE,
+          dayoff: storeInfo.DAYOFF
+        }
+      });
+    } else {
+      throw new Error('Information is missing, please fil out the data');
+    }
   } catch (err) {
-    // if (!stores.NAME || !stores.PRICE) {
-    //   res.status(400).send(err.message);
-    // } else {
-    res.status(400).send(err.message);
-    // }
+    res.status(500).send(err.message);
   }
 };
-
 export default getStoreInfo;
