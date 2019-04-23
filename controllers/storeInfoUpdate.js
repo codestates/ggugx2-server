a97335b3990dd18629a39d30f9809a878f270aa4
IM11 신현수 <kilcmn@gmail.com>
2019-04-23 21:42:06 +0900

    add storeInfoUpdates.js

---
+++ controllers/storeInfoUpdate.js
@@ -0,0 +1,11 @@
+// import db from '../models';
+
+// const storeInfoUpdates = asny(req, res) => {
+//     const { storeId } = req.body.storeId;
+// }
+
+// try {
+//     let storeUpdate = await db.stores.findAll({
+//         attributes: ['address', 'openhour']
+//     })
+// }
