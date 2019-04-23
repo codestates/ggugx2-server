6d81df7ae1fe79b7c71b6531ec4ef4a207775d77
IM11 신현수 <kilcmn@gmail.com>
2019-04-23 21:06:28 +0900

    feature/getStampsRewardsCounts fixed error

--- controllers/getStampsRewardsCounts.js
+++ controllers/getStampsRewardsCounts.js
@@ -6,17 +6,46 @@ const getStampsRewardsCounts = async (req, res) => {
   const { customerID, storeID } = req.body;
 
   try {
-    let stampsData = await db.stamps.findAll({
+    let stampsData = await db.stamps
+      .findAll({
+        where: {
+          [Op.and]: [
+            { CUSTOMER_ID: customerID },
+            { STORE_ID: storeID },
+            { EXCHANGED_DATE: null }
+          ]
+        }
+      })
+      .map(item => item.dataValues);
+
+    let menusData = await db.menus
+      .findAll({
+        where: { STORE_ID: storeID }
+      })
+      .map(item => item.dataValues);
+
+    await db.rewards.findAll({
       where: {
-        [Op.and]: [{ CUSTOMER_ID: customerID }, { STORE_ID: storeID }]
-      }
+        [Op.and]: [
+          { MENU_ID: menusData[0].id },
+          { CUSTOMER_ID: customerID },
+          { USED_DATE: null }
+        ]
+      },
+      order: [['createdAt', 'DESC']]
     });
 
-    let rewardsData = await db.rewards.findAll({
-      where: {
-        [Op.and]: [{ CUSTOMER_ID: customerID }, { USED_DATE: null }]
-      }
-    });
+    let rewardsData = await db.rewards
+      .findAll({
+        where: {
+          [Op.and]: [
+            { MENU_ID: menusData[0].id },
+            { CUSTOMER_ID: customerID },
+            { USED_DATE: null }
+          ]
+        }
+      })
+      .map(item => item.dataValues);
 
     res.status(200).json({
       stamps: stampsData.length,
