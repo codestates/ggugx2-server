9972b41c252b7ea43db079237e9e54bbdaab20d9
Seung-Yoon Oh <feynubrick@gmail.com>
2019-04-26 16:09:21 +0900

    edited to get list of stores upto 5

--- controllers/getNearestStores.js
+++ controllers/getNearestStores.js
@@ -1,6 +1,29 @@
-const getNearestStores = (req, res) => {
+import db from '../models';
+const MAX_NUM_OF_STORES = 5;
+
+const getNearestStores = async (req, res) => {
   console.log('called getNearestStores, body: ', req.body);
-  res.status(200).json(req.body);
+  const { lattitude, longitude } = req.body;
+  console.log(`lattitude: ${lattitude}, longitude: ${longitude}`);
+
+  try {
+    console.log('now searching stores around customer');
+    let result = await db.store
+      .findAll({
+        limit: MAX_NUM_OF_STORES,
+        attributes: [['id', 'storeID'], ['name', 'storeName']]
+      })
+      .map(item => {
+        item.dataValues.distance = 123;
+        return item.dataValues;
+      });
+    console.log('result: ', result);
+
+    res.status(200).json(result);
+  } catch (err) {
+    console.log('ERROR: ', err.message);
+    res.status(500).send(err.message);
+  }
 };
 
 export default getNearestStores;
