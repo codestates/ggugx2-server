759e5642c8b1550b277d0a191dadc993dc1d2b44
IM11 신현수 <kilcmn@gmail.com>
2019-04-27 18:59:24 +0900

    still testing

--- controllers/uploadImage.js
+++ controllers/uploadImage.js
@@ -1,6 +1,7 @@
 import aws from 'aws-sdk';
 import multer from 'multer';
 import multerS3 from 'multer-s3';
+import db from '../models';
 
 const s3 = new aws.S3({
   accessKeyId: 'AKIAJKGQKLF4L7YPH47A',
@@ -13,22 +14,50 @@ const upload = multer({
     s3: s3,
     bucket: 'gguck2-deploy',
     key: function(req, file, cb) {
-      console.log('keyRequest ', req);
-      console.log('file!!! ? : ', file);
+      // console.log('keyRequest ', req);
+      // console.log('file!!! ? : ', file);
       cb(null, Date.now().toString());
     }
   })
 });
 
-const uploadImage = (req,
-res,
-(err, some) => {
-  if (err) {
-    return res
-      .status(422)
-      .send({ errors: [{ title: 'Image Upload Error', detail: err.message }] });
-  }
-  return res.json({ imageUrl: req.file.location });
-});
+const singleUpload = upload.single('image');
+
+const uploadImage = (req, res) => {
+  singleUpload(req, res, (err, some) => {
+    if (err) {
+      return res.status(422).send({
+        errors: [{ title: 'Image Upload Error', detail: err.message }]
+      });
+    }
+    return res.json({ imageUrl: req.file.location });
+  });
+  console.log('!!!!!!!!!!!!!!!!!!!', uploadImage);
+};
+
+// const { isMain, storeId } = req.body;
+// try {
+//   let storeimage = await db.storeimage
+//     .findAll({
+//       where: {
+//         [Op.and]: [{ isMain: isMain }, { storeID: storeId }, { url: null }]
+//       }
+//     })
+//     .map(itme => itme.dataValues);
+
+//   if (storeimage) {
+//     await db.storeImage.update({
+//       url: req.file.location
+//     });
+//     res.status(200).send('url has udated!!!!!!!');
+//   } else {
+//     res.status(200).json({
+//       url: req.file.location
+//     });
+//   }
+// } catch (err) {
+//   throw new Error(err.message);
+// }
+// };
 
 export default uploadImage;
