7c3c9cd16305feb7847562e8ac0f81c1b42f14e8
IM11 신현수 <kilcmn@gmail.com>
2019-04-27 19:44:17 +0900

    testing again

--- controllers/uploadImage.js
+++ controllers/uploadImage.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 import multerS3 from 'multer-s3';
 import db from '../models';
 
+const Op = db.Sequelize.Op;
 const s3 = new aws.S3({
   accessKeyId: 'AKIAJKGQKLF4L7YPH47A',
   secretAccessKey: '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',
@@ -22,42 +23,46 @@ const upload = multer({
 });
 
 const singleUpload = upload.single('image');
+singleUpload(req, res, (err, some) => {
+  if (err) {
+    return res.status(422).send({
+      errors: [{ title: 'Image Upload Error', detail: err.message }]
+    });
+  }
+  return res.json({ imageUrl: req.file.location });
+});
+console.log('!!!!!!!!!!!!!!!!!!!', uploadImage);
 
-const uploadImage = (req, res) => {
-  singleUpload(req, res, (err, some) => {
-    if (err) {
-      return res.status(422).send({
-        errors: [{ title: 'Image Upload Error', detail: err.message }]
-      });
-    }
-    return res.json({ imageUrl: req.file.location });
-  });
-  console.log('!!!!!!!!!!!!!!!!!!!', uploadImage);
-};
+// const uploadImage = async (req, res) => {
+//   const { isMain, storeId } = req.body;
 
-// const { isMain, storeId } = req.body;
-// try {
-//   let storeimage = await db.storeimage
-//     .findAll({
-//       where: {
-//         [Op.and]: [{ isMain: isMain }, { storeID: storeId }, { url: null }]
-//       }
-//     })
-//     .map(itme => itme.dataValues);
+//   try {
+//     let storeimage = await db.storeimage
+//       .findAll({
+//         where: {
+//           [Op.and]: [{ isMain: isMain }, { storeId: storeId }, { url: url }]
+//         }
+//       })
+//       .map(itme => itme.dataValues);
 
-//   if (storeimage) {
-//     await db.storeImage.update({
-//       url: req.file.location
-//     });
-//     res.status(200).send('url has udated!!!!!!!');
-//   } else {
-//     res.status(200).json({
-//       url: req.file.location
-//     });
+//     if (storeimage) {
+//       singleUpload(req, res, (err, some) => {
+//         if (err) {
+//           return res.status(422).send({
+//             errors: [{ title: 'Image Upload Error', detail: err.message }]
+//           });
+//         }
+//         return res.json({ imageUrl: req.file.location });
+//       });
+//       console.log('!!!!!!!!!!!!!!!!!!!', uploadImage);
+//     } else {
+//       res.status(200).json({
+//         url: params.data
+//       });
+//     }
+//   } catch (err) {
+//     throw new Error(err.message);
 //   }
-// } catch (err) {
-//   throw new Error(err.message);
-// }
 // };
 
 export default uploadImage;
