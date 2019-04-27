6de0b7c3a7a01054f3d9e121b3b731dfe56f4e42
Seung-Yoon Oh <feynubrick@gmail.com>
2019-04-27 13:56:21 +0900

    fixed typo in error messages

--- controllers/addMenu.js
+++ controllers/addMenu.js
@@ -1,6 +1,16 @@
-const addMenu = (req, res) => {
+import db from '../models';
+import {} from '../errorMessages';
+
+const addMenu = async (req, res) => {
   console.log('called addMenu with body: ', req.body);
-  res.status(200).json(req.body);
+  const { name, price, storeId } = req.body;
+
+  try {
+    let store = await db.store.findByPk(storeId);
+    res.status(200).json(req.body);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
 
 export default addMenu;
--- controllers/signinStores.js
+++ controllers/signinStores.js
@@ -3,7 +3,7 @@ import { encrypt } from '../modules';
 import {
   PHONENUMBER_OR_PASSWORD_MISSING,
   WRONG_PASSWORD,
-  STORE_NOT_EXITS
+  STORE_NOT_EXISTS
 } from '../errorMessages';
 import db from '../models';
 import { secret, expireTime } from '../config';
@@ -22,7 +22,7 @@ const signinStores = async (req, res) => {
     });
 
     if (!result) {
-      throw new Error(STORE_NOT_EXITS);
+      throw new Error(STORE_NOT_EXISTS);
     }
     //좀 더 편리한 방법
     let { dataValues: store } = result;
@@ -44,7 +44,7 @@ const signinStores = async (req, res) => {
   } catch (err) {
     if (err.message === PHONENUMBER_OR_PASSWORD_MISSING) {
       res.status(400).send(err.message);
-    } else if (err.message === STORE_NOT_EXITS) {
+    } else if (err.message === STORE_NOT_EXISTS) {
       res.status(400).send(err.message);
     } else {
       res.status(500).send(err.message);
