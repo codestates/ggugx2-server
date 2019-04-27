865817cc6736253af075297f217b8390e847de3e
Seung-Yoon Oh <feynubrick@gmail.com>
2019-04-27 14:05:21 +0900

    added menu creation to the route

--- controllers/addMenu.js
+++ controllers/addMenu.js
@@ -1,5 +1,5 @@
 import db from '../models';
-import {} from '../errorMessages';
+import { STORE_NOT_EXISTS } from '../errorMessages';
 
 const addMenu = async (req, res) => {
   console.log('called addMenu with body: ', req.body);
@@ -7,7 +7,17 @@ const addMenu = async (req, res) => {
 
   try {
     let store = await db.store.findByPk(storeId);
-    res.status(200).json(req.body);
+    if (!store) {
+      throw new Error(STORE_NOT_EXISTS);
+    }
+
+    let newMenu = await db.menu.create({
+      name,
+      price
+    });
+    newMenu.setStore(store);
+
+    res.status(201).send('a menu successfully added!');
   } catch (err) {
     res.status(500).send(err.message);
   }
