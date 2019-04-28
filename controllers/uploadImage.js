01ea892dca45feedb4cc8ca8d2925fb174b71527
IM11 신현수 <kilcmn@gmail.com>
2019-04-28 21:02:26 +0900

    able upload to s3 and db and return url

--- controllers/uploadImage.js
+++ controllers/uploadImage.js
@@ -3,7 +3,6 @@ import multer from 'multer';
 import multerS3 from 'multer-s3';
 import db from '../models';
 
-const Op = db.Sequelize.Op;
 const s3 = new aws.S3({
   accessKeyId: 'AKIAJKGQKLF4L7YPH47A',
   secretAccessKey: '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',
@@ -14,55 +13,30 @@ const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: 'gguck2-deploy',
-    key: function(req, file, cb) {
-      // console.log('keyRequest ', req);
-      // console.log('file!!! ? : ', file);
-      cb(null, Date.now().toString());
+    key: function(req, res, cb) {
+      cb(null, `image/${Date.now().toString()}`);
     }
   })
 });
 
 const singleUpload = upload.single('image');
-singleUpload(req, res, (err, some) => {
-  if (err) {
-    return res.status(422).send({
-      errors: [{ title: 'Image Upload Error', detail: err.message }]
-    });
-  }
-  return res.json({ imageUrl: req.file.location });
-});
-console.log('!!!!!!!!!!!!!!!!!!!', uploadImage);
-
-// const uploadImage = async (req, res) => {
-//   const { isMain, storeId } = req.body;
-
-//   try {
-//     let storeimage = await db.storeimage
-//       .findAll({
-//         where: {
-//           [Op.and]: [{ isMain: isMain }, { storeId: storeId }, { url: url }]
-//         }
-//       })
-//       .map(itme => itme.dataValues);
-
-//     if (storeimage) {
-//       singleUpload(req, res, (err, some) => {
-//         if (err) {
-//           return res.status(422).send({
-//             errors: [{ title: 'Image Upload Error', detail: err.message }]
-//           });
-//         }
-//         return res.json({ imageUrl: req.file.location });
-//       });
-//       console.log('!!!!!!!!!!!!!!!!!!!', uploadImage);
-//     } else {
-//       res.status(200).json({
-//         url: params.data
-//       });
-//     }
-//   } catch (err) {
-//     throw new Error(err.message);
-//   }
-// };
+const uploadImage = (req, res) => {
+  singleUpload(req, res, err => {
+    if (req === undefined || req === null) {
+      res.end(err.message);
+    } else if (req) {
+      db.storeImage.create({
+        isMain: req.body.isMain,
+        storeID: req.body.storeId,
+        url: req.file.location
+      });
+      res.status(200).json({
+        url: req.file.location
+      });
+    } else {
+      res.status(500).send('Internal problem');
+    }
+  });
+};
 
 export default uploadImage;
